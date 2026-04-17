c1f93a2cb1ce1d8a2b0efce60307343d8c16370f
Shamil2k7 <164727469+Shamil2k7@users.noreply.github.com>
2026-04-17 16:51:43 +0530

    Delete scrpt.js

--- scrpt.js
+++
@@ -1,8 +0,0 @@
-const box = document.getElementById("storyDiv");
-
-function scrolrigth(){
-  box.scrollBy({ left: 200, behavior: "smooth" });
-}
-function scrolLeft(){
-  box.scrollBy({ left: -200, behavior: "smooth" });
-}
